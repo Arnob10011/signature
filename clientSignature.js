d1c930db9575b046f6f3e7ba7d32b22c4e39f137
Arnob <87138850+Arnob10011@users.noreply.github.com>
2023-08-25 03:30:02 +0600

    Update clientSignature.js

--- clientSignature.js
+++ clientSignature.js
@@ -1,22 +1,82 @@
-import React,{useState} from 'react'
-import SignatureCanvas from 'react-signature-canvas'
-import axios from 'axios'
-import '../../App.css'
+import React, { useRef, useState } from 'react';
+import axios from 'axios';
 
-// use this css. THIS IS A MUST
-// .sigCanvas{
-//   width: 100%;
-//   height: 100%;
-// }
+const Signature = () => {
+  // creating the whole canvas
+  const canvasRef = useRef(null);
+  const [isDrawing, setIsDrawing] = useState(false);
+  const [lastX, setLastX] = useState(0);
+  const [lastY, setLastY] = useState(0);
+
+  const startDrawing = (context, event) => {
+    context.beginPath();
+    context.moveTo(event.clientX, event.clientY);
+    setIsDrawing(true);
+    setLastX(event.clientX);
+    setLastY(event.clientY);
+  };
+
+  const draw = (context, event) => {
+    if (!isDrawing) return;
+
+    const currentX = event.clientX;
+    const currentY = event.clientY;
+
+    // Calculate distance between current and last points
+    const distance = Math.sqrt(
+      Math.pow(currentX - lastX, 2) + Math.pow(currentY - lastY, 2)
+    );
+
+    context.lineCap = 'round';
+    context.lineJoin = 'round';
+
+    // Draw intermediate points if the distance is larger
+    if (distance > 1) {
+      context.beginPath();
+      context.moveTo(lastX, lastY);
+      context.lineTo(currentX, currentY);
+      context.stroke();
+      setLastX(currentX);
+      setLastY(currentY);
+    }
+  };
+
+  const stopDrawing = () => {
+    setIsDrawing(false);
+  };
+
+  const handlePointerDown = (event) => {
+    const canvas = canvasRef.current;
+    const context = canvas.getContext('2d');
+    context.lineWidth = 3; // Set the desired line width here
+
+    startDrawing(context, event);
+  };
+
+  const handlePointerMove = (event) => {
+    const canvas = canvasRef.current;
+    const context = canvas.getContext('2d');
+
+    draw(context, event);
+  };
+
+  const handlePointerUp = () => {
+    stopDrawing();
+  };
 
 
-export default function Signature() {
-  const [sign,setSign] = useState()
-  const clear = () => sign.clear()
+  // other operations work
+
+  const handleClearCanvas = () => {
+    const canvas = canvasRef.current;
+    const context = canvas.getContext('2d');
+    context.clearRect(0, 0, canvas.width, canvas.height);
+  };
 
   const handleSave =async () => {
+    console.log(canvasRef.current.toDataURL('image/png'))
 
-    const blob = dataURLtoBlob(sign.toDataURL('image/png'))
+    const blob = dataURLtoBlob(canvasRef.current.toDataURL('image/png'))
     const formData = new FormData()
     formData.append("signature", blob, 'signature.png')
 
@@ -29,7 +89,7 @@ export default function Signature() {
     })
     console.log(res.data)
     // removes the signature from client side
-    clear()
+    handleClearCanvas()
   };
 
 
@@ -47,33 +107,52 @@ export default function Signature() {
   }
 
 
+
+
   return (
-    <div className='p-5'>
-    <div className='w-auto h-auto border border-red-900'>
-        <SignatureCanvas canvasProps={{ className:"sigCanvas"}}  ref={(data) => setSign(data)} />
+    <div>
+
+<div >
+      <canvas
+        ref={canvasRef}
+        width={900} // Set canvas dimensions as needed
+        height={400}
+        onPointerDown={handlePointerDown}
+        onPointerMove={handlePointerMove}
+        onPointerUp={handlePointerUp}
+      />
     </div>
+
+
 
     <div className=" mt-4 gap-3 flex justify-end">
     
 
-            <button
-              onClick={clear}
-              className="px-4 py-2 text-sm font-medium text-white bg-blue-600 rounded-lg hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-blue-400"
-            >
-              Clear
-            </button>
+    <button
+      onClick={handleClearCanvas}
+      className="px-4 py-2 text-sm font-medium text-white bg-blue-600 rounded-lg hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-blue-400"
+    >
+      Clear
+    </button>
 
-            
-            <button
-              onClick={handleSave}
-              className="px-4 py-2 text-sm font-medium text-white bg-blue-600 rounded-lg hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-blue-400"
-            >
-              save
-            </button>
-          </div>
+    
+    <button
+      onClick={handleSave}
+      className="px-4 py-2 text-sm font-medium text-white bg-blue-600 rounded-lg hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-blue-400"
+    >
+      save
+    </button>
+  </div>
 
 
 
     </div>
-  )
-}
+
+
+    
+
+    
+  );
+};
+
+export default Signature;
